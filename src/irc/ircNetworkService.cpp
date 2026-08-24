69cc86122bab3fb023878a986469a243a5c4c203
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-08-24 14:46:19 -0500

    background network service using asio

--- src/irc/ircNetworkService.cpp
+++ src/irc/ircNetworkService.cpp
@@ -1,1 +1,16 @@
 #include "ircNetworkService.h"
+
+void IRCNetworkService::init() {
+    //start thread that execute jobs and waits for new ones
+    m_io_thread = std::jthread([this]() {
+        m_io_context.run();
+    });
+}
+
+void IRCNetworkService::stop() {
+    m_work_guard.reset();
+}
+
+IRCNetworkService::Executor IRCNetworkService::executor() {
+    return m_io_context.get_executor();
+}
--- src/irc/ircNetworkService.h
+++ src/irc/ircNetworkService.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <thread>
+#include <boost/asio.hpp>
+
+#include "core/logger.h"
 
 /*
  *Owns a boost::asio::io_context, a work guard, and a background std::jthread running io_context.run().
@@ -7,4 +11,25 @@
  */
 
 class IRCNetworkService {
+    using Executor = boost::asio::io_context::executor_type;
+public:
+    //initalize the "network service"
+    void init();
+    //return from "run()"
+    void stop();
+    //get executor
+    Executor executor();
+    //submit "job"
+    template<typename Func>
+    void post(Func&& function);
+private:
+    Logger*                 m_logger = &Logger::get();
+    std::jthread            m_io_thread;
+    boost::asio::io_context m_io_context;
+    boost::asio::executor_work_guard<Executor> m_work_guard{boost::asio::make_work_guard(m_io_context)};
 };
+
+template<typename Func>
+void IRCNetworkService::post(Func&& function) {
+    boost::asio::post( m_io_context, std::function<Func>(function) );
+}
