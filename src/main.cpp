550826a449bbfecf8db50e5a101d5dbeeabb89f7
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-08-24 22:26:59 -0500

    minor rename

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
     Logger* logger = &Logger::get();
 
     if (const std::unique_ptr<Application> application = std::make_unique<Application>(); application->init()) {
-        logger->info("Application constructed at: {}", fmt::ptr(application.get()) );
+        logger->trace("Application constructed at: {}", fmt::ptr(application.get()) );
         application->loop();
     } else {
         logger->critical("Application start-up failed");
