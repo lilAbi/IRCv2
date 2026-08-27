8e0371da27bf36504843bfcd37ec84f2df60d8fb
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-08-26 21:23:16 -0500

    building session manager

--- src/irc/sessionManager.cpp
+++ src/irc/sessionManager.cpp
@@ -1,3 +1,25 @@
-
-
 #include "sessionManager.h"
+
+SessionManager::SessionManager(boost::asio::any_io_executor io_executor, ThreadSafeQueue<NetworkEventVariant>& network_event_queue)
+    : m_io_executor(io_executor), m_network_event_queue(network_event_queue) {
+}
+
+void SessionManager::connect(ServerConfig config) {
+
+}
+
+void SessionManager::disconnect(int server_id) {
+
+}
+
+void SessionManager::join(int server_id, std::string channel) {
+
+}
+
+void SessionManager::leave(int server_id, std::string channel) {
+
+}
+
+void SessionManager::sendMessage(int server_id, std::string target, std::string message) {
+
+}
--- src/irc/sessionManager.h
+++ src/irc/sessionManager.h
@@ -1,5 +1,22 @@
 #pragma once
 
+#include "container/threadSafeQueue.h"
+#include "ircMetadata.h"
+#include "ircSession.h"
+#include <boost/asio.hpp>
+
 class SessionManager {
 public:
+    SessionManager(boost::asio::any_io_executor io_executor, ThreadSafeQueue<NetworkEventVariant>& network_event_queue);
+
+    void connect(ServerConfig config);
+    void disconnect(int server_id);
+    void join(int server_id, std::string channel);
+    void leave(int server_id, std::string channel);
+    void sendMessage(int server_id, std::string target, std::string message);
+
+private:
+    boost::asio::any_io_executor m_io_executor;
+    ThreadSafeQueue<NetworkEventVariant>& m_network_event_queue;
+    std::unordered_map<int, std::shared_ptr<IrcSession>> m_sessions;
 };
