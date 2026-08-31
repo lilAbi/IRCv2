1f83f1c5c4e3c2d45373d04e263a34bf75c1188d
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-08-31 11:10:48 -0500

    building IrcSession abstraction

--- src/irc/ircSession.cpp
+++ src/irc/ircSession.cpp
@@ -1,11 +1,22 @@
 #include "ircSession.h"
 
-IrcSession::IrcSession(boost::asio::any_io_executor io_executor, ThreadSafeQueue<NetworkEventVariant> &network_event_queue)
-    : m_io_executor(io_executor), m_network_event_queue(network_event_queue), m_resolver(io_executor), m_socket(io_executor){
+IrcSession::IrcSession(const boost::asio::any_io_executor& io_executor, ThreadSafeQueue<NetworkEventVariant> &network_event_queue)
+    : m_resolver(io_executor), m_socket(io_executor), m_network_event_queue(network_event_queue) {
 }
 
 void IrcSession::connect(ServerConfig config) {
+    m_logger->trace("Attempting connection to endpoint {}", config.m_host);
+    boost::asio::ip::tcp::endpoint endpoint{ boost::asio::ip::make_address_v4(config.m_host), static_cast<unsigned short>(stoi(config.m_port)) };
+    m_logger->trace("Attempting connection to endpoint {}", config.m_host);
+    //endpoint.address().to_string()
+    //dns lookup
+    /*
+    m_resolver.async_resolve(endpoint, [](const boost::system::error_code& error, const boost::asio::ip::tcp::resolver::results_type& results) {
 
+    });
+    */
+    //m_resolver.resolve()
+    //boost::asio::async_connect()
 }
 
 void IrcSession::disconnect() {
--- src/irc/ircSession.h
+++ src/irc/ircSession.h
@@ -1,15 +1,22 @@
 #pragma once
 
 #include "ircMetadata.h"
+#include "core/logger.h"
 #include "container/threadSafeQueue.h"
 #include <boost/asio.hpp>
 #include <memory>
 #include <deque>
 #include <string>
 
+
+/*
+ *  Represents one IRC connection.
+ *  Manages connection state, and implements the IRC protocol (sending NICK/USER, parsing incoming lines, handling PING, etc.).
+ */
+
 class IrcSession : public std::enable_shared_from_this<IrcSession> {
 public:
-    IrcSession(boost::asio::any_io_executor io_executor, ThreadSafeQueue<NetworkEventVariant>& network_event_queue);
+    IrcSession(const boost::asio::any_io_executor& io_executor, ThreadSafeQueue<NetworkEventVariant>& network_event_queue);
 
     void connect(ServerConfig config);
     void disconnect();
@@ -25,13 +32,13 @@ private:
     void startRead();
     void startWrite();
 private:
-    boost::asio::any_io_executor    m_io_executor{};
     ThreadSafeQueue<NetworkEventVariant>& m_network_event_queue;
+    Logger*                         m_logger = &Logger::get();
     boost::asio::ip::tcp::resolver  m_resolver;
     boost::asio::ip::tcp::socket    m_socket;
-    boost::asio::streambuf          m_read_buffer{};
-    std::deque<std::string>         m_write_queue{};
-    int                             m_server_id;
+    boost::asio::streambuf          m_read_buffer;
+    std::deque<std::string>         m_write_queue;
+    int                             m_server_id{};
     std::string                     m_nick;
     std::string                     m_username;
     std::string                     m_real_name;
