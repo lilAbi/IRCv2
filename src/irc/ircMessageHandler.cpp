fcc416228698c37d9db53084e2819f4e5f30f07e
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-09-11 03:22:04 -0500

    built api to handle irc messages

--- src/irc/ircMessageHandler.cpp
+++ src/irc/ircMessageHandler.cpp
@@ -1,16 +1,42 @@
 #include "ircMessageHandler.h"
 
+#include "util/parseCursor.h"
+
 IrcMessageHandler::IrcMessageHandler(ThreadSafeQueue<NetworkEventVariant> &network_event_queue) : m_network_event_queue(network_event_queue) {
 }
 
-IrcMessage IrcMessageHandler::parseIRCMessage(std::string message) {
+void IrcMessageHandler::processIRCMessage(std::string message) {
+    const auto irc_message = parseIRCMessage(message);
+    dispatchMessage(irc_message);
+}
+
+IrcMessage IrcMessageHandler::parseIRCMessage(std::string& message) {
     IrcMessage irc_message;
-    //if message return empty
-    if (message.empty()) return irc_message;
-
-    if (message.front() == '@') {
-        
-    }
-
+    ParseCursor cursor{message};
+    //todo: finish
     return irc_message;
+}
+
+void IrcMessageHandler::dispatchMessage(const IrcMessage& message) {
+    if (message.m_command == "PING") {
+        this->handlePing(message);
+    }
+    else if (message.m_command == "PRIVMSG") {
+        this->handlePrivateMessage(message);
+    }
+    else if (message.m_command == "JOIN") {
+        this->handleJoin(message);
+    }
+    else if (message.m_command == "PART") {
+        this->handlePart(message);
+    }
+    else if (message.m_command == "NICK") {
+        this->handleNick(message);
+    }
+    else if (message.m_command == "001") {
+        this->handleWelcome(message);
+    }
+    else {
+        this->handleUnknown(message);
+    }
 }
--- src/irc/ircMessageHandler.h
+++ src/irc/ircMessageHandler.h
@@ -6,8 +6,21 @@
 class IrcMessageHandler {
 public:
     explicit IrcMessageHandler(ThreadSafeQueue<NetworkEventVariant>& network_event_queue);
-
-    static IrcMessage parseIRCMessage(std::string message);
+    //processes an incoming irc command from the server
+    void processIRCMessage(std::string message);
+private:
+    //parse incoming string into irc message abstraction
+    IrcMessage parseIRCMessage(std::string& message);
+    //route messages to correct handlers
+    void dispatchMessage(const IrcMessage& message);
+    //handle commands
+    void handlePing(const IrcMessage& message);
+    void handlePrivateMessage(const IrcMessage& message);
+    void handleJoin(const IrcMessage& message);
+    void handlePart(const IrcMessage& message);
+    void handleNick(const IrcMessage& message);
+    void handleWelcome(const IrcMessage& message);
+    void handleUnknown(const IrcMessage& message);
 
 private:
     ThreadSafeQueue<NetworkEventVariant>& m_network_event_queue;
